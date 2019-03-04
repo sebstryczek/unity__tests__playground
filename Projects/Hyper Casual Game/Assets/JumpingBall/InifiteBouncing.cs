4a6a38d15bbcb8e0e1ef0702b2a439c3b97d31ba
sebstryczek <sebastian@stryczek.pl>
2019-03-04 23:41:30 +0100

    Move project to github

---
+++ Projects/Hyper Casual Game/Assets/JumpingBall/InifiteBouncing.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InifiteBouncing : MonoBehaviour
+{
+    private void Update()
+    {
+        if (Input.GetAxis("Horizontal") > 0) {
+            Vector2 curr = this.gameObject.GetComponent<Rigidbody>().velocity;
+            this.gameObject.GetComponent<Rigidbody>().velocity = new Vector2(1, curr.y);
+        }
+    }
+    
+    private void OnCollisionEnter(Collision other)
+    {
+        Debug.Log('!');
+        this.gameObject.GetComponent<Rigidbody>().velocity = new Vector2(0, 3);
+    }
+}
